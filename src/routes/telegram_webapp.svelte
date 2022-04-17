7000d619cd009e1e6dba0c0f976a5bd86ac5ed44
Nikolay Kostyurin <jilizart@gmail.com>
2022-04-17 21:24:25 +0200

    feat: telegram bot web app

---
+++ src/routes/telegram_webapp.svelte
@@ -0,0 +1,41 @@
+
+<svelte:head>
+    <script src="https://telegram.org/js/telegram-web-app.js"></script>
+</svelte:head>
+
+<script>
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        if (window?.Telegram?.WebApp) {
+            window?.Telegram?.WebApp.ready()
+        }
+    })
+
+    function onClick() {
+        if (window?.Telegram?.WebApp) {
+            window.Telegram.WebApp.close()
+        }
+    }
+</script>
+
+<section>
+    <button class="button" on:click={onClick}>
+        Close
+    </button>
+</section>
+
+<style>
+    :global(html) {
+        background-color: var(--tg-theme-bg-color);
+    }
+
+    .button {
+        display: flex;
+        width: 100%;
+        color: var(--tg-theme-button-text-color, white);
+        background: var(--tg-theme-button-color, black);
+        margin: 0 10px;
+        padding: 8px 14px;
+    }
+</style>

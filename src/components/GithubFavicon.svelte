d64fc4312dcee08d87b3fcf7c58ab4b65e9ab171
Nikolay Kostyurin <jilizart@gmail.com>
2023-12-10 11:37:38 +0200

    fix: github favicon

--- src/components/GithubFavicon.svelte
+++ src/components/GithubFavicon.svelte
@@ -9,4 +9,15 @@
         background: url("../assets/github-favicon.svg") 50% 50% no-repeat;
         background-size: 1rem 1rem;
     }
+    @media (prefers-color-scheme: dark) {
+        .github-favicon {
+            filter: invert(100%);
+        }
+    }
+    :global(body.dark-mode) .github-favicon {
+        filter: invert(100%);
+    }
+    :global(body.light-mode) .github-favicon {
+        filter: invert(0);
+    }
 </style>

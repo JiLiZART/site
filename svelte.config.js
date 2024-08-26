24f32a8636256ac9d2359796dccc1f545550110f
Nikolay Kostyurin <jilizart@gmail.com>
2024-08-26 13:16:28 +0200

    fix: build

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,18 @@
-import preprocess from 'svelte-preprocess';
+import { sveltePreprocess } from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 import * as child_process from 'node:child_process';
+
+const version = child_process.execSync('git rev-parse HEAD').toString().trim()
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: sveltePreprocess(),
 
 	kit: {
         version: {
-            name: child_process.execSync('git rev-parse HEAD').toString().trim()
+            name: version
         },
 		// hydrate the <div id="svelte"> element in src/app.html
 		// target: '#svelte',
